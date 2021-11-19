5be7c6319b3631ee9bd607f3f3ffb9ef83feb810
Kristina_Garina <tinka_e@mail.ru>
2021-11-19 19:00:29 +0300

    work on bugs

--- 4.5.prime.js
+++ 4.5.prime.js
@@ -1,14 +1,15 @@
 function showPrimes(n) {
 
-    for (let i = 2; i < n; i++) {
-      if (!isPrime(i)) continue;
-  
-      console.log(i);  
+  for (let i = 2; i < n; i++) {
+    if (!isPrime(i)) continue;
+
+ console.log(i);
   }
-  
-  function isPrime(i) {
-    for (let i = 2; i < n; i++) {
-      if ( n % i == 0) return false;
-    }
-    return true;
-  
+}
+
+function isPrime(n) {
+  for (let i = 2; i < n; i++) {
+    if ( n % i == 0) return false;
+  }
+  return true;
+}
