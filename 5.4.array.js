ec2ae2bf6fc71edc35c2526f6da2baaf96b8ac96
Kristina_Garina <tinka_e@mail.ru>
2021-11-25 22:43:25 +0300

    work on bug

--- 5.4.array.js
+++ 5.4.array.js
@@ -1,8 +1,6 @@
-let arr = [1,2,3,4]
-
-console.log( String(arr) === '1,2,3,4' );
-
-let array = [1,6,7,8,3,4,5,6]
-array.sort((a, b) => b - a );
-
-console.log( array )
+let arr = [0,2,3,4,0,0,0]
+let resultArr = []
+for(let i = 0; i < arr.length; i++){
+  resultArr.push(Boolean(arr[i]))
+}
+console.log(resultArr)
